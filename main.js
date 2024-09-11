ef490acbd113b3a364a3c52eab689eabbf91bb16
Kado12 <gonzalosh01@gmail.com>
2024-09-11 14:24:04 -0500

    Server Básico

---
+++ main.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+app.post("/pruebas", async (req, res) => {
+  try {
+    console.log(req.body)
+    console.log(req.data)
+    console.log('Hola mundo')
+    res.status(200).json(req.body)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+
+app.listen(3000, () => {
+  console.log("Server listen on port 3000");
+
+});
+
+// {{message_text}}
+
+// {{lead.id}}
