1785600c94f2e3120c1a3c2fccf1d255283ead4b
Kado12 <gonzalosh01@gmail.com>
2024-09-11 14:45:00 -0500

    Vercel

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hola mundo' });
+});
+
 app.post("/pruebas", async (req, res) => {
   try {
     console.log(req.body)
