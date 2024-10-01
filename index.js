f2708384fbe5e73095831caaca90ffe739e3932d
Kado12 <gonzalosh01@gmail.com>
2024-10-01 13:07:30 -0500

    Add 4kJuego

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const apiKey = process.env.OPENAI_API_KEY
 const assistantId = process.env.ASSISTANT_ID
 const assistantId4K = process.env.ASSISTANT_ID_ELEMENTO4K
+const assistantId4KJuego = process.env.ASSISTANT_ID_ELEMENTO4K_JUEGO
 
 const express = require('express')
 const OpenAI = require('openai')
@@ -12,15 +13,15 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 // Preguntar a Chat GPT
-async function chatCompletions() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "Cuentame un poema corto" }],
-    model: "gpt-3.5-turbo",
-  });
+// async function chatCompletions() {
+//   const completion = await openai.chat.completions.create({
+//     messages: [{ role: "system", content: "Cuentame un poema corto" }],
+//     model: "gpt-3.5-turbo",
+//   });
 
-  console.log(completion.choices[0]);
-  console.log('Respuesta: ' + completion.choices[0].message.content);
-}
+//   console.log(completion.choices[0]);
+//   console.log('Respuesta: ' + completion.choices[0].message.content);
+// }
 
 // Crear Thread
 async function createdThread() {
@@ -352,6 +353,132 @@ app.post("/4k", async (req, res) => {
     console.log(error)
     let url = req.body.return_url
     let token = process.env.TOKEN_WIDGET_ELEMENTO4K
+    const response = await fetch(`${url}`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      },
+      body: JSON.stringify({
+        data: {
+          status: "failed"
+        }
+      })
+    })
+    res.status(500).json({ message: 'Error al procesar la solicitud' })
+  }
+})
+
+
+// IA Entretenimiento Elemento4K
+app.post("/4kJuego", async (req, res) => {
+  try {
+    if (req.headers["user-agent"] === "amoCRM-Webhooks/3.0") {
+      console.log(req.body.data)
+      let name_client = req.body.data.name_client
+      let thread_id = req.body.data.thread_id
+      let url = req.body.return_url
+      let token = process.env.TOKEN_WIDGET_ELEMENTO4K_JUEGO
+      let responseAI
+
+      let msj_complete = `
+        Su mensaje es: ${req.body.data.msj_1} ${req.body.data.msj_2} ${req.body.data.msj_3}
+        `
+      if (msj_complete.includes('Voice message') || msj_complete.includes('messageContextInfo')) {
+        if (thread_id) {
+          await deletedThread(thread_id)
+        }
+        thread_id = ' '
+        const response = await fetch(`${url}`, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+          },
+          body: JSON.stringify({
+            data: {
+              status: "success",
+              msj: " ",
+              flujo: "detener",
+              threadId: thread_id
+            }
+          })
+        })
+        res.sendStatus(200)
+      }
+
+      if (name_client) {
+        msj_complete = `
+        El nombre del cliente es: ${name_client} \n
+        Su mensaje es: ${req.body.data.msj_1} ${req.body.data.msj_2} ${req.body.data.msj_3} ${req.body.data.msj_4} ${req.body.data.msj_5} \n
+        `
+      }
+      let content = msj_complete
+      if (!thread_id) {
+        thread_id = await createdThread()
+      }
+      if (thread_id) {
+        responseAI = await main(content, thread_id, assistantId4KJuego)
+      } else {
+        const response = await fetch(`${url}`, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+          },
+          body: JSON.stringify({
+            data: {
+              status: "success",
+              msj: " ",
+              flujo: "detener",
+              threadId: " ",
+            }
+          })
+        })
+        res.sendStatus(200)
+      }
+      console.log(responseAI)
+      let objectJSON = JSON.parse(responseAI)
+      let LM = objectJSON.respuesta
+      let flujo = objectJSON.flujo
+
+      console.log('responseAI a JSON - Respuesta: ' + objectJSON.respuesta)
+      console.log('responseAI a JSON - Asesor: ' + objectJSON.flujo)
+
+      if (flujo == 'detener') {
+        await deletedThread(thread_id)
+        thread_id = ' '
+      }
+
+
+      const response = await fetch(`${url}`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + token
+        },
+        body: JSON.stringify({
+          data: {
+            status: "success",
+            msj: LM,
+            flujo: flujo,
+            threadId: thread_id
+          }
+        })
+      })
+      res.sendStatus(200)
+    } else {
+      res.sendStatus(200)
+    }
+
+  } catch (error) {
+    console.log(error)
+    let url = req.body.return_url
+    let token = process.env.TOKEN_WIDGET_ELEMENTO4K_JUEGO
     const response = await fetch(`${url}`, {
       method: 'POST',
       headers: {
